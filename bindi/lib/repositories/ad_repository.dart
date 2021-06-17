34d447c3f97212c576ae25f8ce2cc8a3fff5c875
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-06-17 16:10:18 -0300

    CM - Salvando as Imagens no Parse
    Salvando as Imagens no Parse

--- bindi/lib/repositories/ad_repository.dart
+++ bindi/lib/repositories/ad_repository.dart
@@ -1,5 +1,37 @@
+import 'dart:io';
+
 import 'package:bindi/models/ad.dart';
+import 'package:bindi/repositories/parse_errors.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:path/path.dart' as path;
 
 class AdRepository {
-  void save(Ad ad) {}
+  Future<void> save(Ad ad) async {
+    final paseImages = await saveImages(ad.images);
+  }
+
+  Future<List<ParseFile>> saveImages(List images) async {
+    final parseImages = <ParseFile>[];
+    try {
+      for (final image in images) {
+        if (image is File) {
+          final parseFile = ParseFile(image, name: path.basename(image.path));
+          final response = await parseFile.save();
+          if (!response.success) {
+            return Future.error(
+                ParseErrors.getDescription(response.error.code));
+          }
+          parseImages.add(parseFile);
+        } else {
+          final parseFile = ParseFile(null);
+          parseFile.name = path.basename(image);
+          parseFile.url = image;
+          parseImages.add(parseFile);
+        }
+      }
+      return parseImages;
+    } catch (e) {
+      return Future.error('Falha ao salvar imagens');
+    }
+  }
 }
