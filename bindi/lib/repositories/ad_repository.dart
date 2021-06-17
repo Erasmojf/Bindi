3292dfd8e2b9954eb8e5c2e0b58fc67c94c21a90
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-06-17 17:17:02 -0300

    CM - Salvando o anuncio no Parse
    Salvando o anuncio no Parse

--- bindi/lib/repositories/ad_repository.dart
+++ bindi/lib/repositories/ad_repository.dart
@@ -2,12 +2,41 @@ import 'dart:io';
 
 import 'package:bindi/models/ad.dart';
 import 'package:bindi/repositories/parse_errors.dart';
+import 'package:bindi/repositories/table_keys.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:path/path.dart' as path;
 
 class AdRepository {
   Future<void> save(Ad ad) async {
     final paseImages = await saveImages(ad.images);
+
+    final parseUser = ParseUser('', '', '')..set(keyUserId, ad.user.id);
+
+    final adObject = ParseObject(keyAdTable);
+
+    final parseAcl = ParseACL(owner: parseUser);
+    parseAcl.setPublicReadAccess(allowed: true);
+    parseAcl.setPublicWriteAccess(allowed: false);
+    adObject.setACL(parseAcl);
+    adObject.set<String>(keyAdTitle, ad.title);
+    adObject.set<String>(keyAdDescription, ad.description);
+    adObject.set<bool>(keyAdHidePhone, ad.hidePhone);
+    adObject.set<num>(keyAdPrice, ad.price);
+    adObject.set<int>(keyAdStatus, ad.status.index);
+
+    adObject.set<String>(keyAdDistrict, ad.address.distriict);
+    adObject.set<String>(keyAdCity, ad.address.city.name);
+    adObject.set<String>(keyAdFederativeUnit, ad.address.uf.initials);
+    adObject.set<String>(keyAdPostalCode, ad.address.cep);
+
+    adObject.set<List<ParseFile>>(keyAdImages, paseImages);
+    adObject.set<ParseUser>(keyAdOwner, parseUser);
+
+    adObject.set<ParseObject>(keyAdCategory,
+        ParseObject(keyCategoryTable)..set(keyCategoryId, ad.category.id));
+
+    final response = await adObject.save();
+    print(response.success);
   }
 
   Future<List<ParseFile>> saveImages(List images) async {
