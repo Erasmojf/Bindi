787298e9b5b6c518a35f2258b5467c3f2e64642d
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-06-21 10:25:08 -0300

    CM - Criando o campo de pesquisa
    Criando o campo de pesquisa

--- bindi/lib/screens/home/home_screen.dart
+++ bindi/lib/screens/home/home_screen.dart
@@ -1,13 +1,32 @@
 import 'package:bindi/components/custom_drawer/custom_drawer.dart';
 import 'package:flutter/material.dart';
 
+import 'components/search_dialog.dart';
+
 class HomeScreen extends StatelessWidget {
+  openSearch(BuildContext context) async {
+    final search = await showDialog(
+        context: context,
+        builder: (_) => SearchDialog(
+              currentSearch: 'Text',
+            ));
+    print(search);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         drawer: CustomDrawer(),
-        appBar: AppBar(),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  openSearch(context);
+                })
+          ],
+        ),
       ),
     );
   }
