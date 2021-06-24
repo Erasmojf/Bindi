e840a745e09a3c7170cdbc97325209b2c88b9bfc
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-06-23 23:59:26 -0300

    CM - Botao animado de inserir anuncio
    Botao animado de inserir anuncio

--- bindi/lib/screens/home/home_screen.dart
+++ bindi/lib/screens/home/home_screen.dart
@@ -5,11 +5,20 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
 
 import 'components/ad_tile.dart';
+import 'components/create_ad_button.dart';
 import 'components/search_dialog.dart';
 import 'components/top_bar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final HomeStore homeStore = GetIt.I<HomeStore>();
+
+  final ScrollController scrollController = ScrollController();
+
   openSearch(BuildContext context) async {
     final search = await showDialog(
       context: context,
@@ -66,80 +75,91 @@ class HomeScreen extends StatelessWidget {
           children: [
             TopBar(),
             Expanded(
-              child: Observer(
-                builder: (_) {
-                  if (homeStore.error != null)
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.error,
-                            color: Colors.white,
-                            size: 100,
+              child: Stack(
+                children: [
+                  Observer(
+                    builder: (_) {
+                      if (homeStore.error != null)
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.error,
+                                color: Colors.white,
+                                size: 100,
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                'Ocorreu um erro!',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 20,
+                                ),
+                              )
+                            ],
                           ),
-                          const SizedBox(height: 8),
-                          Text(
-                            'Ocorreu um erro!',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 20,
+                        );
+                      if (homeStore.showProgress)
+                        return Center(
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation(
+                              Colors.white,
                             ),
-                          )
-                        ],
-                      ),
-                    );
-                  if (homeStore.showProgress)
-                    return Center(
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation(
-                          Colors.white,
-                        ),
-                      ),
-                    );
-                  if (homeStore.adList.isEmpty)
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.border_clear,
-                            color: Colors.white,
-                            size: 100,
                           ),
-                          const SizedBox(height: 8),
-                          Text(
-                            'Ops...Nenhum anúncio encontrado!',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 20,
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  return ListView.builder(
-                    itemCount: homeStore.itemCount,
-                    itemBuilder: (_, index) {
-                      if (index < homeStore.adList.length)
-                        return AdTile(homeStore.adList[index]);
+                        );
+                      if (homeStore.adList.isEmpty)
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.border_clear,
+                                color: Colors.white,
+                                size: 100,
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                'Ops...Nenhum anúncio encontrado!',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 20,
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      return ListView.builder(
+                        controller: scrollController,
+                        itemCount: homeStore.itemCount,
+                        itemBuilder: (_, index) {
+                          if (index < homeStore.adList.length)
+                            return AdTile(homeStore.adList[index]);
 
-                      homeStore.loadNextPage();
-                      return Container(
-                        height: 10,
-                        child: LinearProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation(
-                            Colors.purple,
-                          ),
-                        ),
+                          homeStore.loadNextPage();
+                          return Container(
+                            height: 10,
+                            child: LinearProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation(
+                                Colors.purple,
+                              ),
+                            ),
+                          );
+                        },
                       );
                     },
-                  );
-                },
+                  ),
+                  Positioned(
+                    bottom: -50,
+                    left: 0,
+                    right: 0,
+                    child: CreateAdButton(scrollController),
+                  )
+                ],
               ),
             ),
           ],
