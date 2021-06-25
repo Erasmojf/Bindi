55d42632061714f8b186e9602d8ba8089387e9f4
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-06-25 19:25:32 -0300

    CM - Menu com opcoes de editar, vendido e excluir
    Menu com opcoes de editar, vendido e excluir

--- bindi/lib/screens/myads/components/active_tile.dart
+++ bindi/lib/screens/myads/components/active_tile.dart
@@ -7,6 +7,12 @@ class ActiveTile extends StatelessWidget {
 
   final Ad ad;
 
+  final List<MenuChoice> choices = [
+    MenuChoice(index: 0, title: 'Editar', iconData: Icons.edit),
+    MenuChoice(index: 1, title: 'Já vendi', iconData: Icons.thumb_up),
+    MenuChoice(index: 2, title: 'Excluid', iconData: Icons.delete),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -47,9 +53,9 @@ class ActiveTile extends StatelessWidget {
                       ),
                     ),
                     Text(
-                      '${ad.views}',
+                      '${ad.views} Visitas',
                       style: TextStyle(
-                        fontSize: 11,
+                        fontSize: 12,
                         color: Colors.grey[800],
                       ),
                     )
@@ -57,9 +63,60 @@ class ActiveTile extends StatelessWidget {
                 ),
               ),
             ),
+            Column(
+              children: [
+                PopupMenuButton<MenuChoice>(
+                  onSelected: (choice) {
+                    switch (choice.index) {
+                      case 0:
+                        break;
+                      case 1:
+                        break;
+                      case 2:
+                        break;
+                    }
+                  },
+                  icon: Icon(Icons.more_vert, size: 20),
+                  itemBuilder: (_) {
+                    return choices
+                        .map(
+                          (choice) => PopupMenuItem<MenuChoice>(
+                            value: choice,
+                            child: Row(
+                              children: [
+                                Icon(
+                                  choice.iconData,
+                                  size: 20,
+                                  color: Colors.purple,
+                                ),
+                                const SizedBox(width: 8),
+                                Text(
+                                  choice.title,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.purple,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                        .toList();
+                  },
+                ),
+              ],
+            )
           ],
         ),
       ),
     );
   }
+}
+
+class MenuChoice {
+  MenuChoice({this.index, this.title, this.iconData});
+
+  final int index;
+  final String title;
+  final IconData iconData;
 }
